60ac8d4f570e3a96b268f474cf164d9d5d9f4dce
Chaewon Yoon <ycw0363@gmail.com>
2024-12-01 17:09:27 +0900

    Add Lecture 02 Memorize

---
+++ Lectures/Lecture02/02 Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Chaewon on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
