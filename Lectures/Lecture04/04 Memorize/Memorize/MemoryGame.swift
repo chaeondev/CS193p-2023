d4028e5af5dc1995eb05ca705143897c0d8c2761
Chaewon Yoon <ycw0363@gmail.com>
2024-12-12 21:32:39 +0900

    Add Lecture 04 Memorize

---
+++ Lectures/Lecture04/04 Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,39 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Chaewon on 12/8/24.
+//
+
+import Foundation
+
+// Model
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        // add numberOfPairsOfCards x 2 cards
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) {
+            let content: CardContent = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+        print(cards)
+    }
+    
+    struct Card {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        let content: CardContent
+        
+    }
+}
